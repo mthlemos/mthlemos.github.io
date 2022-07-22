9847470d57c1e1e6b7f564c0ce384d27485fd245
Matheus Lemos <matheus.l.c@outlook.com>
2022-07-22 18:25:06 -0300

    Cleanup and update packages
    - Update Astro to beta 73
    - Reenable typescript checking during build
    - Remove partytown to reduce dependencies on final build
    - Remove unused astro-fonts-next package
    - Make ProjectCards component idle, as it doesn't need to be interactive
      right away

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(),
-  partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  })],
+  integrations: [react(), tailwind()],
   site: "https://mthlemos.xyz"
 });
