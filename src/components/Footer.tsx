4755941f101dc491efafb836f8e3979d01449295
Matheus Lemos <37716097+mthlemos@users.noreply.github.com>
2024-11-19 11:42:00 +0000

    Update Footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC<Props> = ({ version }) => {
     return (
         <div className="mt-16 py-10 w-full min-w-[62.5rem] box-border mx-0 border-gray-dark border-t-2 bg-gradient-to-b from-gray-card to-gray-light">
             <p className="text-off-white font-custom-sans text-center">Matheus Bastos Lemos Portfolio</p>
-            <p className="text-off-white font-custom-sans text-center">Built by me © 2022</p>
+            <p className="text-off-white font-custom-sans text-center">Built by me © 2024</p>
             <p className="text-off-white font-custom-sans text-center">Powered by React, Tailwind CSS and Astro</p>
             <p className="text-xs text-gray-600 font-custom-mono text-center">{`v ${version}`}</p>
         </div>
